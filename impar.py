7465f807c8d755412261a4944887b9427955b2e6
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-01 14:37:46 -0300

    adicionando comentario

--- impar.py
+++ impar.py
@@ -1,15 +1,1 @@
 # Módulos
-from random import choice
-
-# Quantidade de números
-tamanho = 8
-
-# Lista
-numeros = (1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25)
-
-sorteados = set()
-
-while len(sorteados) < tamanho:
-    sorteados.add(choice(numeros))
-    # lista.sort()
-print(sorteados)
